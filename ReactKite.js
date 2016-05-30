1698f24ed8ee11d857088cda40e2fd419cf5ac94
Peter Swallow <Peter@peters-mbp.lan>
2016-05-30 17:11:34 +0100

    Added styles to text Input control

--- ReactKite.js
+++ ReactKite.js
@@ -26,12 +26,34 @@ var ReactKite = React.createClass(
   
   render() {
     return (
-      <View>
-        <Text>You input {this.state.zip}</Text>
-        <TextInput onSubmitEditing={this._handleTextChange} />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>You input {this.state.zip}</Text>
+        <TextInput style={styles.input} onSubmitEditing={this._handleTextChange} />
       </View>
     );
   }
 });
+
+var styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF'
+    },
+    welcome:{
+      fontSize:20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    input:
+    {
+      fontSize: 20,
+      borderWidth: 2,
+      height:40
+    }
+  }
+)
 
 module.exports = ReactKite;
